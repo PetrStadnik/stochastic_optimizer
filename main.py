d29f5da851957da36ef6a17b914dee8243c59aa5
ps <peta.stadnik@email.cz>
2025-09-18 10:46:11 +0200

    qantiles

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from testing_data.tasks import TasksCreator
+from testing_data.tasks import TasksCreator, DynamicTask
 from solver.solver import Solver
 from solver.orknapsack import Knapsack
 from solver.orilp import IlpKnapsack
@@ -7,38 +7,41 @@ from solver.orilp_adaptive import IlpKnapsackAdaptive
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import ScalarFormatter
+from scipy.stats import norm
 
-HORIZONT = 2*60*60 # 2h
+
+def quantile(dt: [DynamicTask], success_probability):
+    for task in dt:
+        task.estimated_duration = norm.ppf(success_probability, loc=task.mean, scale=task.std)
+
+    return dt
+
 
 if __name__ == '__main__':
     print("Hello! \n All times are in seconds!!!")
     tc = TasksCreator()
-    periodic_tasks = tc.return_periodic_tasks()
-    dynamic_tasks = tc.return_dynamic_tasks_v3()
-    """
-    print("Periodic tasks:")
-    for i in periodic_tasks: print(i)
-    print("Dynamic tasks:")
-    for i in dynamic_tasks: print(i)
-    """
 
-    solver = Solver()
-    iteration = 0
     status = True
     real_dur_sum = []
     obj_val_list = []
     run_time_list = []
     capacity = 1800
+    exc = 0
     print("### LP ###")
     for i in range(1000):
-        dynamic_tasks = tc.return_dynamic_tasks_v4(i+1)
+        print(i)
+        dynamic_tasks = tc.return_dynamic_tasks_v5(i+1)
+        dynamic_tasks = quantile(dynamic_tasks, success_probability=0.95)
         sum_of_real_dur, obj_val, run_time =IlpKnapsackAdaptive(capacity=capacity).solve(dynamic_tasks)
         real_dur_sum.append(sum_of_real_dur)
         obj_val_list.append(obj_val)
         run_time_list.append(run_time)
+        if sum_of_real_dur > capacity:
+            exc += 1
 
     print(f"TOTAL TIME EXCEEDED: {sum([i-capacity for i in real_dur_sum if i > capacity])}s")
     print(f"TOTAL UNUSED TIME: {sum([capacity-i for i in obj_val_list if i < capacity])}s")
+    print(f"INSTANCES EXCEEDED: {exc}/1000 ~ {100*exc/1000}%")
 
     """ VISUALISATION """
     data1 = np.array(real_dur_sum)
